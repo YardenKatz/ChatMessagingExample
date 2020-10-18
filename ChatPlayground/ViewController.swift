4dca11051b8f549675a765487ca7c0cc36c332f6
Yarden Katz <kyarden@cambium.co.il>
2020-10-18 17:01:59 +0300

    Initial Commit

---
+++ ChatPlayground/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatPlayground
+//
+//  Created by David Salzer on 18/10/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
